d3a17303b10e6e6250980f4b5ae019a710d9ceae
KingsIke <ogbonnayakingsike@gmail.com>
2026-09-21 11:31:56 +0100

    fix: merge platform migration branches

---
+++ api/platform/migrations/0016_merge_20260921_1115.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("platform", "0015_merge_20260918_1620"),
+        ("platform", "0015_platformsettings_auto_flag_after_hours_and_more"),
+    ]
+
+    operations = []
